84b2c30c3586ec89a35736670846b85418571286
Dewn Nwed <dewn49@gmail.com>
2024-08-10 17:20:06 +0700

    Change theme color

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -27,7 +27,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "5706f08d96bca1e08bd9722b8e466ae0
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "3dc683db340acf12cf427c929e36604c",
+"flutter_bootstrap.js": "5511e38f74c277341270c2aabe86dadb",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
